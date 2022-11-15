2e907b91159d365c353c39091b928f0a7efe6ed5
HanYan2517 <117310257+HanYan2517@users.noreply.github.com>
2022-11-15 01:33:33 -0500

    This is a test commit for 1-100

---
+++ NewFile.py
@@ -0,0 +1,13 @@
+i = 0
+while(i <= 100):
+    if i%3==0 and i%15!=0:
+        print('Fizz')
+    elif i%5 ==0 and i%15!=0:
+        print('Buzz')
+    elif i%15==0:
+        print('FizzBuzz')
+    else:
+        print(i)
+   
+   
+    i=i+1
