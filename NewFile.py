bfad3f51ca2283b28903eb4ddcd8e9f0e818bf63
HanYan2517 <117310257+HanYan2517@users.noreply.github.com>
2022-11-15 23:56:51 -0500

    Exercise1

--- NewFile.py
+++ NewFile.py
@@ -1,13 +1,20 @@
-i = 0
-while(i <= 100):
-    if i%3==0 and i%15!=0:
-        print('Fizz')
-    elif i%5 ==0 and i%15!=0:
-        print('Buzz')
-    elif i%15==0:
-        print('FizzBuzz')
-    else:
-        print(i)
-   
-   
-    i=i+1
+##FizzBuzz
+'''
+def FizzBuzz():
+    i = 0
+    while(i <= 100):
+        if i%3==0 and i%15!=0:
+            print('Fizz')
+        elif i%5 ==0 and i%15!=0:
+            print('Buzz')
+        elif i%15==0:
+            print('FizzBuzz')
+        else:
+            print(i)
+           
+        i=i+1
+'''
+
+##Prime number
+def Prime(n):
+    
